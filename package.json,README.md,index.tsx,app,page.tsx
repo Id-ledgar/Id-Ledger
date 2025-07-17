e8e2f296c5ef3fa8b96b0c3ba90e1c86cb631a24
Id-ledgar <220467565+Id-ledgar@users.noreply.github.com>
2025-07-17 15:17:21 +0100

    add package.janson and initial setup

---
+++ package.json,README.md,index.tsx,app,page.tsx
@@ -0,0 +1,31 @@
+{
+  "name": "id-ledger",
+  "version": "1.0.0",
+  "description": "ID Ledger - Virtual accountant for UK self-employed and LTD companies",
+  "main": "index.js",
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start",
+    "lint": "next lint"
+  },
+  "keywords": ["accounting", "UK", "tax", "self-employed", "LTD", "bookkeeping"],
+  "author": "Mariusz Radziwillowicz",
+  "license": "MIT",
+  "dependencies": {
+    "next": "14.2.3",
+    "react": "18.2.0",
+    "react-dom": "18.2.0",
+    "axios": "^1.6.7",
+    "date-fns": "^3.6.0",
+    "zustand": "^4.5.2"
+  },
+  "devDependencies": {
+    "typescript": "^5.4.3",
+    "@types/node": "^20.11.30",
+    "@types/react": "^18.2.42",
+    "@types/react-dom": "^18.2.17",
+    "eslint": "8.56.0",
+    "eslint-config-next": "14.2.3"
+  }
+}
